913052bc0a1641cf27277911586dadd5e2c13d49
Emilio Granell Romero <egranell@dsic.upv.es>
2015-02-09 18:45:03 +0100

    first commit

---
+++ app/src/main/java/com/aemus/Read4SpeechExperiments/settings.java
@@ -0,0 +1,14 @@
+package com.aemus.Read4SpeechExperiments;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class settings extends PreferenceActivity {
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.settings);
+    }
+}
